aec6a704dc375024255dd2d06d46a0f2d696969e
karina-carmonaa <karina.carmonav@hotmail.com>
2019-09-08 22:57:31 -0500

    asa

--- config/database.php
+++ config/database.php
@@ -65,7 +65,6 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env(''),
             'host' => env('DB_HOST', 'ec2-174-129-226-232.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'd5pnk6fa8s7tj8'),
